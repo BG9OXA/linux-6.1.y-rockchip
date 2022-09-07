66f3e44a6f16770ca8781f057fd40411d734d050
Vinayak Yadawad <vinayak.yadawad@broadcom.com>
2022-09-07 18:14:48 +0530

    cfg80211: Update Transition Disable policy during port authorization
    [ Upstream commit 0ff57171d6d225558c81a69439d5323e35b40549 ]
    
    In case of 4way handshake offload, transition disable policy
    updated by the AP during EAPOL 3/4 is not updated to the upper layer.
    This results in mismatch between transition disable policy
    between the upper layer and the driver. This patch addresses this
    issue by updating transition disable policy as part of port
    authorization indication.
    
    Signed-off-by: Vinayak Yadawad <vinayak.yadawad@broadcom.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Stable-dep-of: a519be2f5d95 ("wifi: mac80211: do not use old MBSSID elements")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/uapi/linux/nl80211.h
+++ include/uapi/linux/nl80211.h
@@ -2752,6 +2752,8 @@ enum nl80211_commands {
  * @NL80211_ATTR_WIPHY_ANTENNA_GAIN: Configured antenna gain. Used to reduce
  *	transmit power to stay within regulatory limits. u32, dBi.
  *
+ * @NL80211_ATTR_TD_BITMAP: Transition Disable bitmap, for subsequent
+ *	(re)associations.
  * @NUM_NL80211_ATTR: total number of nl80211_attrs available
  * @NL80211_ATTR_MAX: highest attribute number currently defined
  * @__NL80211_ATTR_AFTER_LAST: internal use
@@ -3279,6 +3281,7 @@ enum nl80211_attrs {
 
 	NL80211_ATTR_TX_HW_TIMESTAMP,
 	NL80211_ATTR_RX_HW_TIMESTAMP,
+	NL80211_ATTR_TD_BITMAP,
 
 	NL80211_ATTR_WIPHY_ANTENNA_GAIN,
 
