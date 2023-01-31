d57c9776f6caaa5b5b7cc4fafc9a6016c84a4f4c
Aloka Dixit <quic_alokad@quicinc.com>
2023-01-30 16:12:25 -0800

    wifi: nl80211: validate and configure puncturing bitmap
    [ Upstream commit d7c1a9a0ed180d8884798ce97afe7283622a484f ]
    
    - New feature flag, NL80211_EXT_FEATURE_PUNCT, to advertise
      driver support for preamble puncturing in AP mode.
    - New attribute, NL80211_ATTR_PUNCT_BITMAP, to receive a puncturing
      bitmap from the userspace during AP bring up (NL80211_CMD_START_AP)
      and channel switch (NL80211_CMD_CHANNEL_SWITCH) operations. Each bit
      corresponds to a 20 MHz channel in the operating bandwidth, lowest
      bit for the lowest channel. Bit set to 1 indicates that the channel
      is punctured. Higher 16 bits are reserved.
    - New members added to structures cfg80211_ap_settings and
      cfg80211_csa_settings to propagate the bitmap to the driver after
      validation.
    
    Signed-off-by: Aloka Dixit <quic_alokad@quicinc.com>
    Signed-off-by: Muna Sinada <quic_msinada@quicinc.com>
    Link: https://lore.kernel.org/r/20230131001227.25014-3-quic_alokad@quicinc.com
    [move validation against 0xffff into policy]
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Stable-dep-of: a519be2f5d95 ("wifi: mac80211: do not use old MBSSID elements")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/uapi/linux/nl80211.h
+++ include/uapi/linux/nl80211.h
@@ -2754,6 +2754,12 @@ enum nl80211_commands {
  *
  * @NL80211_ATTR_TD_BITMAP: Transition Disable bitmap, for subsequent
  *	(re)associations.
+ *
+ * @NL80211_ATTR_PUNCT_BITMAP: (u32) Preamble puncturing bitmap, lowest
+ *	bit corresponds to the lowest 20 MHz channel. Each bit set to 1
+ *	indicates that the sub-channel is punctured. Higher 16 bits are
+ *	reserved.
+ *
  * @NUM_NL80211_ATTR: total number of nl80211_attrs available
  * @NL80211_ATTR_MAX: highest attribute number currently defined
  * @__NL80211_ATTR_AFTER_LAST: internal use
@@ -3284,6 +3290,8 @@ enum nl80211_attrs {
 	NL80211_ATTR_TD_BITMAP,
 
 	NL80211_ATTR_WIPHY_ANTENNA_GAIN,
+
+	NL80211_ATTR_PUNCT_BITMAP,
 
 	/* add attributes here, update the policy in nl80211.c */
 
@@ -6299,6 +6307,8 @@ enum nl80211_feature_flags {
  *	might apply, e.g. no scans in progress, no offchannel operations
  *	in progress, and no active connections.
  *
+ * @NL80211_EXT_FEATURE_PUNCT: Driver supports preamble puncturing in AP mode.
+ *
  * @NUM_NL80211_EXT_FEATURES: number of extended features.
  * @MAX_NL80211_EXT_FEATURES: highest extended feature index.
  */
@@ -6367,6 +6377,7 @@ enum nl80211_ext_feature_index {
 	NL80211_EXT_FEATURE_FILS_CRYPTO_OFFLOAD,
 	NL80211_EXT_FEATURE_RADAR_BACKGROUND,
 	NL80211_EXT_FEATURE_POWERED_ADDR_CHANGE,
+	NL80211_EXT_FEATURE_PUNCT,
 
 	/* add new features before the definition below */
 	NUM_NL80211_EXT_FEATURES,
