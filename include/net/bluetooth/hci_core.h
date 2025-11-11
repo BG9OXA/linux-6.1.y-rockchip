352830be4bbbce4fc3ef49d0ea9063dad965b45e
Arseniy Krasnov <avkrasnov@salutedevices.com>
2025-11-11 10:41:28 +0800

    Bluetooth: hci_sync: fix double free in 'hci_discovery_filter_clear()'
    [ Upstream commit 2935e556850e9c94d7a00adf14d3cd7fe406ac03 ]
    
    Function 'hci_discovery_filter_clear()' frees 'uuids' array and then
    sets it to NULL. There is a tiny chance of the following race:
    
    'hci_cmd_sync_work()'
    
     'update_passive_scan_sync()'
    
       'hci_update_passive_scan_sync()'
    
         'hci_discovery_filter_clear()'
           kfree(uuids);
    
           <-------------------------preempted-------------------------------->
                                               'start_service_discovery()'
    
                                                 'hci_discovery_filter_clear()'
                                                   kfree(uuids); // DOUBLE FREE
    
           <-------------------------preempted-------------------------------->
    
          uuids = NULL;
    
    To fix it let's add locking around 'kfree()' call and NULL pointer
    assignment. Otherwise the following backtrace fires:
    
    [ ] ------------[ cut here ]------------
    [ ] kernel BUG at mm/slub.c:547!
    [ ] Internal error: Oops - BUG: 00000000f2000800 [#1] PREEMPT SMP
    [ ] CPU: 3 UID: 0 PID: 246 Comm: bluetoothd Tainted: G O 6.12.19-kernel #1
    [ ] Tainted: [O]=OOT_MODULE
    [ ] pstate: 60400005 (nZCv daif +PAN -UAO -TCO -DIT -SSBS BTYPE=--)
    [ ] pc : __slab_free+0xf8/0x348
    [ ] lr : __slab_free+0x48/0x348
    ...
    [ ] Call trace:
    [ ]  __slab_free+0xf8/0x348
    [ ]  kfree+0x164/0x27c
    [ ]  start_service_discovery+0x1d0/0x2c0
    [ ]  hci_sock_sendmsg+0x518/0x924
    [ ]  __sock_sendmsg+0x54/0x60
    [ ]  sock_write_iter+0x98/0xf8
    [ ]  do_iter_readv_writev+0xe4/0x1c8
    [ ]  vfs_writev+0x128/0x2b0
    [ ]  do_writev+0xfc/0x118
    [ ]  __arm64_sys_writev+0x20/0x2c
    [ ]  invoke_syscall+0x68/0xf0
    [ ]  el0_svc_common.constprop.0+0x40/0xe0
    [ ]  do_el0_svc+0x1c/0x28
    [ ]  el0_svc+0x30/0xd0
    [ ]  el0t_64_sync_handler+0x100/0x12c
    [ ]  el0t_64_sync+0x194/0x198
    [ ] Code: 8b0002e6 eb17031f 54fffbe1 d503201f (d4210000)
    [ ] ---[ end trace 0000000000000000 ]---
    
    Fixes: ad383c2c65a5 ("Bluetooth: hci_sync: Enable advertising when LL privacy is enabled")
    Signed-off-by: Arseniy Krasnov <avkrasnov@salutedevices.com>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    [ Minor context change fixed. ]
    Signed-off-by: Alva Lan <alvalan9@foxmail.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/net/bluetooth/hci_core.h
+++ include/net/bluetooth/hci_core.h
@@ -29,7 +29,7 @@
 #include <linux/idr.h>
 #include <linux/leds.h>
 #include <linux/rculist.h>
-
+#include <linux/spinlock.h>
 #include <net/bluetooth/hci.h>
 #include <net/bluetooth/hci_sync.h>
 #include <net/bluetooth/hci_sock.h>
@@ -92,6 +92,7 @@ struct discovery_state {
 	u16			uuid_count;
 	u8			(*uuids)[16];
 	unsigned long		name_resolve_timeout;
+	spinlock_t		lock;
 };
 
 #define SUSPEND_NOTIFIER_TIMEOUT	msecs_to_jiffies(2000) /* 2 seconds */
@@ -899,6 +900,7 @@ static inline void iso_recv(struct hci_conn *hcon, struct sk_buff *skb,
 
 static inline void discovery_init(struct hci_dev *hdev)
 {
+	spin_lock_init(&hdev->discovery.lock);
 	hdev->discovery.state = DISCOVERY_STOPPED;
 	INIT_LIST_HEAD(&hdev->discovery.all);
 	INIT_LIST_HEAD(&hdev->discovery.unknown);
@@ -912,9 +914,12 @@ static inline void hci_discovery_filter_clear(struct hci_dev *hdev)
 	hdev->discovery.result_filtering = false;
 	hdev->discovery.report_invalid_rssi = true;
 	hdev->discovery.rssi = HCI_RSSI_INVALID;
+
+	spin_lock(&hdev->discovery.lock);
 	hdev->discovery.uuid_count = 0;
 	kfree(hdev->discovery.uuids);
 	hdev->discovery.uuids = NULL;
+	spin_unlock(&hdev->discovery.lock);
 }
 
 bool hci_discovery_active(struct hci_dev *hdev);
