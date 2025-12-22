d91c49aaca063e4337807f43d3a5c1df95f283fd
Jai Luthra <jai.luthra@ideasonboard.com>
2025-12-22 13:45:29 +0530

    media: i2c: ov5647: Fix PIXEL_RATE value for VGA mode
    [ Upstream commit c063632b494b02e891442d10f17e37b7fcfab9b3 ]
    
    The pixel rate for VGA (640x480) mode is configured in the mode's table
    to be 58.333 MPix/s instead of 55 MPix/s, so fix it.
    
    Fixes: 911f4516ee2b ("media: ov5647: Support V4L2_CID_PIXEL_RATE")
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/all/CAPY8ntA2TCf9FuB6Nk%2BOn%2By6N_PMuYPAOAr3Yx8YESwe4skWvw@mail.gmail.com/
    Suggested-by: Dave Stevenson <dave.stevenson@raspberrypi.com>
    Signed-off-by: Jai Luthra <jai.luthra@ideasonboard.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Hans Verkuil <hverkuil+cisco@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/media/i2c/ov5647.c
+++ drivers/media/i2c/ov5647.c
@@ -615,7 +615,7 @@ static const struct ov5647_mode ov5647_modes[] = {
 			.width		= 2560,
 			.height		= 1920,
 		},
-		.pixel_rate	= 55000000,
+		.pixel_rate	= 58333000,
 		.hts		= 1852,
 		.vts		= 0x1f8,
 		.link_freq_index = 3,
